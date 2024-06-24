6b4d3bb526317244578e73a7f877a6be040d4b10
JiseokLee <jisuk500@gmail.com>
2024-06-24 20:55:45 +0900

    add test codes

---
+++ test/main_test.cpp
@@ -0,0 +1,15 @@
+
+// #include <cuvoxmap/
+#include <iostream>
+
+int main()
+{
+    // cuvoxmap::CuvoxMap::init_s init;
+    // init.a = 2;
+    // init.b = 1;
+    // cuvoxmap::CuvoxMap map(init);
+    // // map.only_at_gpu();
+
+    // std::cout << map.is_using_gpu() << std::endl;
+    return 0;
+}
